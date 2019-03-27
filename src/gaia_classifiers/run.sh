9d1bd0ecd01d969f691acf0dcfa306d996144c8c
Pablo Alonso <pablo.alonso@upf.edu>
2019-03-27 14:01:01 +0100

    Extract features by default

--- src/gaia_classifiers/run.sh
+++ src/gaia_classifiers/run.sh
@@ -7,6 +7,6 @@ exp_dir=${PWD}/exp/
 
 cd "${0%/*}"
 
-# ${interpreter} -u extract_features.py ${data_dir} -t yaml -s
+${interpreter} -u extract_features.py ${data_dir} -t yaml -s
 
 ${interpreter} -u train_classifiers.py ${data_dir} -o ${exp_dir} -t 10
