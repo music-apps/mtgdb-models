5206c87a74cb0c011881954620659ba31a5eaccf
Pablo Alonso <pablo.alonso@upf.edu>
2019-03-27 12:33:36 +0100

    Implement force option and improve log information

--- src/gaia_classifiers/train_classifiers.py
+++ src/gaia_classifiers/train_classifiers.py
@@ -3,6 +3,7 @@
 import os
 import yaml
 import traceback
+import shutil
 
 from argparse import ArgumentParser
 from gaia2.scripts.classification import train_model
@@ -18,7 +19,7 @@ def get_files_in_dir(dirname, extension):
 
 
 def match_groudtruth(dataset_data_dir, filelist_file, groundtruth_input_file,
-                     groundtruth_output_file, tolerance=5):    
+                     groundtruth_output_file, log, tolerance=5):
     # List .sig files 
     sig_files = [f for f in get_files_in_dir(
         dataset_data_dir, "sig")]
@@ -30,7 +31,7 @@ def match_groudtruth(dataset_data_dir, filelist_file, groundtruth_input_file,
 
     # Get groundtruth entries
     if not os.path.exists(groundtruth_input_file):
-        raise Exception('not valid metadata file for {}.'.format(
+        raise Exception('not valid metadata file for {}'.format(
             groundtruth_input_file))
 
     gt_dict = yaml.load(open(groundtruth_input_file))
@@ -53,17 +54,24 @@ def match_groudtruth(dataset_data_dir, filelist_file, groundtruth_input_file,
 
     if gt_not_in_ls == 0:
         gt_dict['groundTruth'] = gt
-        print('Every track in the ground truth was found.')
+        log_str = 'Every track in the ground truth was found'
+        print(log_str)
+        log.write('{}\n'.format(log_str))
 
     else:
-        print('Warnning: {} ({}%) of the tracks in the ground truth are missing.'.format(
-            gt_not_in_ls, gt_not_in_ls_per))
+        log_str = 'Warnning: {} ({}%) of the tracks in the ground truth are missing'.format(
+            gt_not_in_ls, gt_not_in_ls_per)
+        print(log_str)
+        log.write('{}\n'.format(log_str))
 
         gt_dict['groundTruth'] = dict(
             filter(lambda i: i[0] in gt_in_ls, gt.iteritems()))
 
     if ls_not_in_gt > 0:
-        print('{} .sig files are not present in the groundtruth.'.format(ls_not_in_gt))
+        log_str = '{} .sig files are not present in the groundtruth'.format(ls_not_in_gt)
+        print(log_str)
+        log.write('{}\n'.format(log_str))
+
         ls = dict(filter(lambda i: i[0] in gt_in_ls, ls.iteritems()))
 
     # Write ground truth and filelist files
@@ -75,11 +83,12 @@ def match_groudtruth(dataset_data_dir, filelist_file, groundtruth_input_file,
 
     if gt_not_in_ls_per > tolerance:
         max_allowed = int(gt_tracks * tolerance // 100)
-        raise Exception('{} ({}%) files from the groundtruth are not listed in the data directory. Maximun missing files allowed amount is {} ({}%).'.format(
+        raise Exception('{} ({}%) files from the groundtruth are not listed in the data directory. '
+			'Maximun missing files allowed amount is {} ({}%).'.format(
             gt_not_in_ls, gt_not_in_ls_per, max_allowed, tolerance))
 
 
-def train_models(data_dir, exp_dir, tolerance=5):
+def train_models(data_dir, exp_dir, tolerance=5, force=False):
     for dataset, groundtruth_input_file in DATASETS_GROUNDTRUTH.items():
         try:
             dataset_data_dir = os.path.join(data_dir, dataset)
@@ -93,19 +102,49 @@ def train_models(data_dir, exp_dir, tolerance=5):
             results_model_file = os.path.join(
                 dataset_exp_dir, "{}.history".format(dataset))
             log_file = os.path.join(dataset_exp_dir, "{}.log".format(dataset))
+            ranking_file = '{}.results.ranking'.format(results_model_file)
+
+            log_str = '\n\n{}\nProcessing "{}"\n{}\n\n'.format(
+                '*' * 30, dataset, '*' * 30)
+            print(log_str)
+
+            log_lines = []
+            log_lines.append(log_str)
+
+            if os.path.exists(dataset_exp_dir):
+                log_str = 'Dataset directory exists...'
+                print(log_str)
+                log_lines.append(log_str)
+
+                if force:
+                    log_str = 'Force flag. "{}" will be deleted and retrained!'.format(dataset)
+                    print(log_str)
+                    log_lines.append(log_str)
+                    shutil.rmtree(dataset_exp_dir)
+                else:
+                    if os.path.exists(ranking_file):
+                        log_str = 'Ranking file exists. Nothing to be done for this dataset!'
+                        print(log_str)
+                        log_lines.append(log_str)
+                        continue
+                    else:
+                        log_str = 'Ranking file does not exit. Training will be resumed'
+                        print(log_str)
+                        log_lines.append(log_str)
+            else:
+                log_str = 'Dataset directory does not exist...'
+                print(log_str)
+                log_lines.append(log_str)
 
             if not os.path.exists(dataset_exp_dir):
+                log_str = 'Creating dataset directory'
+                print(log_str)
+                log_lines.append(log_str)
                 os.makedirs(dataset_exp_dir)
 
-            open(log_file, 'w')
+            log = open(log_file, 'w')
+            log.write('\n'.join(log_lines) + '\n')
 
-            print('\n\n{}\nProcessing "{}"\n{}\n\n"'.format(
-                '*' * 30, dataset, '*' * 30))
-            with open(log_file, 'a') as f:
-                f.write('\n\n{}\nProcessing "{}"\n{}\n\n"'.format(
-                    '*' * 30, dataset, '*' * 30))
-
-            # Convert .json to .sig if required
             json_files = [f for f in get_files_in_dir(
                 dataset_data_dir, "json")]
             yaml_files = [f for f in get_files_in_dir(dataset_data_dir, "sig")]
@@ -116,8 +155,10 @@ def train_models(data_dir, exp_dir, tolerance=5):
                 yaml_files_missing = {os.path.splitext(os.path.basename(f))[0]: os.path.join(
                     dataset_data_dir, f) for f in json_files if f.rstrip('.json') not in yaml_files_existing}
 
-                print("{} json files have to be converted into yamls. {} already exist.".format(
+                log_str = ("{} json files have to be converted into yamls. {} already exist".format(
                     len(yaml_files_missing), len(yaml_files_existing)))
+                print(log_str)
+                log.write('{}\n'.format(log_str))
 
                 filelist_to_convert_file = os.path.join(
                     dataset_exp_dir, "filelist-to-convert.yaml")
@@ -134,18 +175,17 @@ def train_models(data_dir, exp_dir, tolerance=5):
                 MTGDB_DIR, 'stable', groundtruth_input_file)
 
             match_groudtruth(dataset_data_dir, filelist_file, groundtruth_input_file,
-                             groundtruth_output_file, tolerance=tolerance)
+                             groundtruth_output_file, log, tolerance=tolerance)
+            log_str = 'Groundtruth matching done\nStarting the model training'
+            print(log_str)
+            log.write('{}\n'.format(log_str))
+            log.close()
 
             train_model.trainModel(groundtruth_output_file, filelist_file,
                                    project_file, dataset_exp_dir, results_model_file)
-
         except:
             print('Exception occured processing {}'.format(dataset))
             traceback.print_exc()
-
-            with open(log_file, 'w') as f:
-                f.write('Exception occured processing {}'.format(dataset))
-                traceback.print_exc(file=f)
 
 
 if __name__ == '__main__':
@@ -157,7 +197,10 @@ if __name__ == '__main__':
                                 help='Directory to perform the experiments and store the results.')
     argumentParser.add_argument('--tolerance', '-t', default=5, type=float,
                                 help='Allowed percentage of missing groundtruth files.')
+    argumentParser.add_argument('--force', '-f', action='store_true',
+                                help='Recompute existing models.')
 
     args = argumentParser.parse_args()
 
-    train_models(args.data_dir, args.exp_dir, args.tolerance)
+    train_models(args.data_dir, args.exp_dir, tolerance=args.tolerance, force=args.force)
+
