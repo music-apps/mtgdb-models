9a52625ac9c051204bc9bf4f649f9ed3974e350f
Pablo Alonso <pablo.alonso@upf.edu>
2019-03-27 11:55:48 +0100

    Improve exp/ and data/ default location and add -u flag for unbuffered output in Python

--- src/gaia_classifiers/run.sh
+++ src/gaia_classifiers/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-interpreter=python2.7 
+interpreter=python2.7  
 
-data_dir=data/
-exp_dir=exp/
+data_dir=${PWD}/data/
+exp_dir=${PWD}/exp/
 
 cd "${0%/*}"
 
-${interpreter} extract_features.py ${data_dir} -t yaml -s
+# ${interpreter} -u extract_features.py ${data_dir} -t yaml -s
 
-${interpreter} train_classifiers.py ${data_dir} -o ${exp_dir} -t 10
+${interpreter} -u train_classifiers.py ${data_dir} -o ${exp_dir} -t 10
