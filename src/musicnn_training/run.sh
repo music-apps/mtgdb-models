40b6492c2f223eb375a994863efd1b6ec050b2ba
pabloEntropia <pablo.alonso@upf.edu>
2019-09-27 18:10:43 +0200

    Update main bash script

--- src/musicnn_training/run.sh
+++ src/musicnn_training/run.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
+interpreter=python
+with_gpu=1
 
-#SBATCH -J features
-#SBATCH -p high
-#SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
-#SBATCH --mem=16G
-#SBATCH --output=out
-#STATCH --error=err
-
+data_dir=/home/palonso/data/ccetl/essentia
+exp_dir=/home/palonso/models/ccetl/vgg_baseline_den
 
 source ~/venvs/py3.6_musicnn/bin/activate
 
-interpreter=python
-
-data_dir=/homedtic/palonso/data/tlcce_musicnn_baseline
-exp_dir=/homedtic/palonso/models/tlcce_musicnn_baseline
-
-${interpreter} -u run.py ${data_dir} ${exp_dir} -s -n 2
-
+${interpreter} -u run.py ${data_dir} ${exp_dir} -n 5 --with_gpu --seed 6 -te
