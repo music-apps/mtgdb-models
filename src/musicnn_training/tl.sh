2ea0a2c478818b6211870b666553b5fd01da9a11
pabloEntropia <pablo.alonso@upf.edu>
2019-10-01 12:42:50 +0200

    Fix bug on configure_training call

--- src/musicnn_training/tl.sh
+++ src/musicnn_training/tl.sh
@@ -3,9 +3,9 @@
 interpreter=python
 with_gpu=1
 
-data_dir=/home/palonso/data/tlcce_musicnn_tl
-exp_dir=/home/palonso/models/tlcce_musicnn_tl
-model_dir=/home/palonso/reps/mtgdb-models/src/musicnn_training/weights/MTT_musicnn/
+data_dir=/home/palonso/data/ccetl/librosa
+exp_dir=/home/palonso/models/ccetl/vgg_tl_MTT
+model_dir=/home/palonso/reps/mtgdb-models/src/musicnn_training/weights/MTT_vgg/
 source ~/venvs/py3.6_musicnn/bin/activate
 
-${interpreter} -u tl.py ${data_dir} ${exp_dir} ${model_dir} -n 5 --with_gpu --seed 6 -fte
+${interpreter} -u tl.py ${data_dir} ${exp_dir} ${model_dir} 2 100 -n 5 --with_gpu --seed 6 -te
